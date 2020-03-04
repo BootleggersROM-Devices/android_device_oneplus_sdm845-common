cda73b2ce3f7b365205818e013833d861d44d8d4
AyanC1 <choudhury.ayan13@gmail.com>
2020-03-04 16:25:50 +0000

    Revert "Devicesettings: Make it for booty"
    This reverts commit 161ff26c88d291bcb7264d29e411bb1a7a29aee9.

--- DeviceSettings/Android.mk
+++ DeviceSettings/Android.mk
@@ -29,8 +29,8 @@ LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.core_core \
-    androidx.preference_preference
-#    VendorSupport-preference
+    androidx.preference_preference \
+    VendorSupport-preference
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
@@ -46,5 +46,4 @@ LOCAL_RESOURCE_DIR := $(package_resource_overlays) $(LOCAL_RESOURCE_DIR)
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 LOCAL_MODULE_TAGS := optional
 include frameworks/base/packages/SettingsLib/common.mk
-include packages/apps/Settings/Android.mk
 include $(BUILD_PACKAGE)
